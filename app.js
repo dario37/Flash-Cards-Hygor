1db42d3412a9541ad22770bab4c63f8c0a9d230f
dario37 <hygor.dario@escola.pr.gov.br>
2025-09-16 10:24:35 -0300

    16/09

---
+++ app.js
@@ -0,0 +1,19 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `  
+    <div class="cartao__conteudo">
+<h3>${categoria}</h3>
+ <div class="cartao__conteudo__pergunta">
+ <p>${pergunta}<p/>
+   </div>
+ <div class="cartao__conteudo__resposta">
+  <p>RESPOSTA- escrever a resposta</p> 
+     </div>
+</div>`
+
+
+
+}
