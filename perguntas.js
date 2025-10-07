4edf8210a27524cb21b73938b4af441668a0735e
dario37 <hygor.dario@escola.pr.gov.br>
2025-10-07 10:29:32 -0300

    profw

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,5 @@
 criaCartao(
-    'brazil',
+    'brasil',
     'Qual é o maior estado do Brasil em termos de área territorial?',
     'O Amazonas é o maior estado brasileiro em área territorial, com mais de 1,5 milhão de km²'
 )
@@ -7,19 +7,19 @@ criaCartao(
 
 
 criaCartao(
-    'brazil',
+    'brasil',
     'Quem foi o primeiro presidente do Brasil após a Proclamação da República em 1889?'
     'foi Marechal Deodoro da fonseca'
 )
 
 criaCartao(
-    'brazil',
+    'brasil',
     'O que significa o termo "Brasil" em relação ao nome do país? ',
     'O nome "Brasil" vem do pau-brasil, uma árvore nativa da região costeira, cujo extrato era muito utilizado para a produção de tintura vermelha. '
 )
 
 criaCartao(
-    'brazil',
+    'brasil',
     'Quais são as cores da bandeira brasileira e o que elas simbolizam?',
     'As cores da bandeira brasileira são: Verde: representa a casa real de Bragança (família do imperador Dom Pedro II). Amarelo: representa a casa real de Habsburgo (família de Dona Leopoldina, esposa de Dom Pedro I). Azul: simboliza o céu do Rio de Janeiro na noite de 15 de novembro de 1888'
 )
